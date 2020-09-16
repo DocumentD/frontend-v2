7069596ce54470dee3aea00ac4f195aa2b9b422a
Sebastian Espei <seblsebastian@aol.de>
2020-09-16 16:37:49 +0200

    Generate nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
+import { NavigationBarComponent } from './component/navigation-bar/navigation-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavigationBarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
