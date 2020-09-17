678273d3eb87a1286eb6cc1c71c8c8fabc100c4b
Sebastian Espei <seblsebastian@aol.de>
2020-09-17 17:52:23 +0200

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './component/search/search.component';
+import { LoginComponent } from './component/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'search',
+    component: SearchComponent,
+    canActivate: [],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
