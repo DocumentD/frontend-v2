4d2b41f6c8324181d143e033bdba673fe1f1f7b2
Sebastian Espei <seblsebastian@aol.de>
2020-09-17 17:52:15 +0200

    Generate search and login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
 import { NavigationBarComponent } from './component/navigation-bar/navigation-bar.component';
+import { SearchComponent } from './component/search/search.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationBarComponent],
+  declarations: [AppComponent, NavigationBarComponent, SearchComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
