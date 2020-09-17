7951546720a8c551d4c1b9cf34dbd03435db638a
Sebastian Espei <seblsebastian@aol.de>
2020-09-17 19:03:13 +0200

    Add authorization service

--- src/app/service/authorization.service.ts
+++ src/app/service/authorization.service.ts
@@ -1,9 +1,71 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../entity/user';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthorizationService {
+  public token = '';
+  private user: User = null;
+  private isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject<
+    boolean
+  >(false);
+  private loggedInUser: BehaviorSubject<User> = new BehaviorSubject<User>(
+    this.user
+  );
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private cookieService: CookieService
+  ) {}
+
+  public async login(username: string, password: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.http
+        .post<User>(
+          environment.apiEndpoint + '/login',
+          { username, password },
+          { observe: 'response' }
+        )
+        .subscribe(
+          (resp) => {
+            this.handleUserDataResponse(resp.body);
+            this.cookieService.set('Token', resp.headers.get('Token'));
+            resolve();
+          },
+          (error) => {
+            this.logout();
+            reject();
+          }
+        );
+    });
+  }
+
+  public async logout(): Promise<void> {
+    this.cookieService.delete('token');
+    this.isAuthenticated.next(false);
+    this.loggedInUser.next(null);
+    this.user = null;
+    this.router.navigate(['login']);
+  }
+
+  public isLoggedIn(): boolean {
+    return this.isAuthenticated.getValue();
+  }
+
+  public getLoggedInUser(): Observable<User> {
+    return this.loggedInUser.asObservable();
+  }
+
+  private handleUserDataResponse(data: User): void {
+    this.user = data;
+    this.isAuthenticated.next(true);
+    this.loggedInUser.next(this.user);
+  }
 }
