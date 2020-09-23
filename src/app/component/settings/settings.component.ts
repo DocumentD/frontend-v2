53f74cdf21acb776a778327060a10a1a66fa22c8
Sebastian Espei <seblsebastian@aol.de>
2020-09-23 02:56:00 +0200

    Add tabs construct

--- src/app/component/settings/settings.component.ts
+++ src/app/component/settings/settings.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/entity/user';
+import { AuthorizationService } from '../../service/authorization.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  loggedInUser: User;
 
-  constructor() { }
+  constructor(
+    private authorizationService: AuthorizationService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.authorizationService
+      .getLoggedInUser()
+      .subscribe((u) => (this.loggedInUser = u));
   }
-
 }
