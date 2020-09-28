ee6e5ee7c80d938100e58663a5f05f132d5aef6e
Sebastian Espei <seblsebastian@aol.de>
2020-09-28 03:33:41 +0200

    Open own user edit

--- src/app/component/navigation-bar/navigation-bar.component.ts
+++ src/app/component/navigation-bar/navigation-bar.component.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { User } from 'src/app/entity/user';
 import { AuthorizationService } from '../../service/authorization.service';
 import { DocumentService } from '../../service/document.service';
 import { DocumentEditComponent } from '../document-edit/document-edit.component';
+import { UserEditComponent } from '../user-edit/user-edit.component';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -14,6 +16,7 @@ export class NavigationBarComponent implements OnInit {
   isLoggedIn = false;
   displayName = '';
   isCurrentUploading = false;
+  private loggedInUser: User = null;
 
   constructor(
     private http: HttpClient,
@@ -27,9 +30,17 @@ export class NavigationBarComponent implements OnInit {
       .isLoggedInAsObservable()
       .subscribe((b) => (this.isLoggedIn = b));
 
-    this.authorizationService
-      .getLoggedInUser()
-      .subscribe((u) => (this.displayName = u ? u.username : ''));
+    this.authorizationService.getLoggedInUser().subscribe((u) => {
+      this.loggedInUser = u;
+      this.displayName = u ? u.username : '';
+    });
+  }
+
+  openOwnUserDialog(): void {
+    const dialogRef = this.dialog.open(UserEditComponent, {
+      minWidth: '600px',
+      data: this.loggedInUser,
+    });
   }
 
   openFileDialog(): void {
